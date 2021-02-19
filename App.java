d9314865b3ef9b528d9ca216a23c65ef6105e59b
Dominik0319 <dominik.skrobisz1@gmail.com>
2021-02-19 17:04:37 +0100

    first commit

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+      Myclass App2 = new Myclass(); 
+      System.out.println(App2.name+" to "+App2.race+", który ma "+App2.age+" lat" );
+
+    }
+}
---
+++ Myclass.java
@@ -0,0 +1,7 @@
+public class Myclass {
+     
+        String name = "Azor";
+        String race= "labrador";
+        int age =5;
+    }
+
